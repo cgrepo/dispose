901b70776696ee3b12f63ddb7e39f81bfebf9b5e
polken <genesis62@hotmail.com>
2017-01-03 21:35:54 +0000

    BASIC AUTHENTICATION

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,27 @@
+class SessionsController < ApplicationController
+skip_before_action :ensure_login, only: [:new, :create]
+  def new
+   	#only render the login form
+    respond_to do |format|
+      format.html { render  :layout => false  } 
+    end 
+  end
+
+  def create
+   	user = User.find_by(name: params[:user][:name])
+   	password = params[:user][:password]
+   	if user && user.authenticate(password)
+   	 	session[:user_id] = user.id
+   	 	redirect_to root_path, notice: "INICIO DE SESSION EXITOSA!"   	 	
+   	else
+   	 	redirect_to login_path, alert: "INVALIDO"
+   	end
+  end
+
+  def destroy
+   	# only active when logout
+    reset_session
+    redirect_to login_path, notice: "You have been log out"
+  end
+  
+end
